95673412150afd1da94ceca65cdbc247050bcc62
mbrsagor <mbrsagor@gmail.com>
2020-07-14 17:25:55 +0600

    Array basic operation

---
+++ array/index.swift
@@ -0,0 +1,17 @@
+
+var names = ["Shimul", "Sagor", "Lokman", "Faruk"]
+print(names); // list of all names
+print(names[0]); // position of names
+names[2] = "Misho"; // Update array element
+print(names);
+names.append("Limon"); // Add new element last child
+print(names);
+names.insert("Saif", at:1); // Add new element any position
+print(names);
+names.remove(at:2); // Remove element and position
+print(names);
+names.removeFirst(); // Remove first item
+print(names);
+names.removeLast(); // Remove last item
+print(names);
+
