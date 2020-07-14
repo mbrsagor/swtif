391c24ee17a9f8a5d5f2db0c574b7ad1baac68d2
mbrsagor <mbrsagor@gmail.com>
2020-07-14 15:18:08 +0600

    Swift data type

--- data_type.swift
+++ data_type.swift
@@ -7,6 +7,11 @@ print("My Name is: "+name);
 var age = 35; // Integer type
 print("My age is: ",age);
 
-let weight = 53.50;
-print("My weight: ",weight); // Float type
+let weight = 53.50; // Double type
+print("My weight: ",weight); 
 
+let is_coding = true; // Boolean type
+print(is_coding);
+
+let key = "S"; // Character type
+print(key);
