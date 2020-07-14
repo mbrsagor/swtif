0404e2097cc06205d7952284cc990f6819e31f28
mbrsagor <mbrsagor@gmail.com>
2020-07-14 15:06:33 +0600

    Swift first commit

---
+++ data_type.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+
+let name = "Hello I'm Sagor!";  // String type
+print("My Name is: "+name);
+
+var age = 35; // Integer type
+print("My age is: ",age);
+
+let weight = 53.50;
+print("My weight: ",weight); // Float type
+
