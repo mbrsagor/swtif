b812aae5e5b8f0806d5c5b1c0951d618e5ce9e11
mbrsagor <mbrsagor@gmail.com>
2020-07-14 15:50:18 +0600

    Arithmetic Operator

---
+++ basic/operators.swift
@@ -0,0 +1,28 @@
+/*
+Operator in swift:
+    Arithmetic Operators
+    Comparison Operators
+    Logical Operators
+    Bitwise Operators
+    Assignment Operators
+    Range Operators
+    Misc Operators
+*/
+
+// Arithmetic Operator
+var x = 30;
+var y = 20;
+var z = x+y;
+print("Total addition: ",z);
+
+z = x-y;
+print("Total subtraction: ",z)
+
+z = x*y;
+print("Total multiplication: ",z)
+
+z = x/y;
+print("Total divide: ",z)
+
+z = x%y;
+print("Total Modulus: ",z)
