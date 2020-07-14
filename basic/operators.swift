ad58180e455094da47b4f953e655018df6bf13f4
mbrsagor <mbrsagor@gmail.com>
2020-07-14 16:01:33 +0600

    Comparison Operator

--- basic/operators.swift
+++ basic/operators.swift
@@ -26,3 +26,26 @@ print("Total divide: ",z)
 
 z = x%y;
 print("Total Modulus: ",z)
+
+
+
+// Comparison Operator
+var x = 30;
+var y = 20;
+var z = x==y; // equal
+print(z);
+
+z = 10 != 45; // equal or not
+print(z);
+
+z = x > y; // greater than
+print(z);
+
+z = x < y; // less than
+print(z);
+
+z = x >= y; // greater than equal
+print(z);
+
+z = x <= y; // less than equal
+print(z);
