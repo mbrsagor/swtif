76f11a163e60b2c22e41b9f9fd820d90ac95252e
mbrsagor <mbrsagor@gmail.com>
2020-07-18 11:31:14 +0600

    Factorail in Swift

---
+++ basic/factorial.swift
@@ -0,0 +1,11 @@
+func factorial(_ n: Int) -> Int {
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+}
+
+print(factorial(5))
