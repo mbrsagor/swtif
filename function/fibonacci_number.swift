95621eac57ecdcbf4e4d2391e2710b1b55873076
mbrsagor <mbrsagor@gmail.com>
2020-07-18 12:45:17 +0600

    fibonacci number in swift

---
+++ function/fibonacci_number.swift
@@ -0,0 +1,11 @@
+func fib(_ n: Int) -> Int {
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    } else {
+        return fib(n - 1) + fib(n - 2)
+    }
+}
+
+print(fib(20))
